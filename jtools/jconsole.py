15350695a9351a715d0285bfa801b1ba4308d7e1
Jeremy Dean <jdean3003@gmail.com>
2019-08-08 23:26:46 -0400

    Changed jconsole to use the colorama package for formatting text with color.
    Changed jconsole colors to be functions that return a string which will print in a given color, rather than using color constants.
    
    changed jdir.norm() to also remove invalid characters from paths. i.e. '?, <, >'

--- jtools/jconsole.py
+++ jtools/jconsole.py
@@ -1,67 +1,89 @@
 from collections.abc import Iterable
+import colorama
+from colorama import Fore
 
-# console escape sequences for changing display settings.
-white = '\033[0;30m'
-gray = '\033[0;37m'
-blue = '\033[0;34m'
-green = '\033[0;32m'
-cyan = '\033[0;36m'
-red = '\033[0;31m'
-purple = '\033[0;35m'
-yellow = '\033[0;33m'
-underline = '\033[4m'
-bold = '\033[1m'
-endc = '\033[0m'  # reset console formatting
-colors = (white, gray, blue, green, cyan, red,
-          purple, yellow, underline, bold, endc)
+#initliaze colorama so colors can be used in console text
+colorama.init()
+#reset formatting
+endc = '\033[0m'
+
+#return a string that will print the passed parameter in a given color
+def white(text):
+    return Fore.WHITE + str(text) + endc
+
+def blue(text):
+    return Fore.BLUE + str(text) + endc
+
+def green(text):
+    return Fore.GREEN + str(text) + endc
+
+def cyan(text):
+    return Fore.CYAN + str(text) + endc
+
+def red(text):
+    return Fore.RED + str(text) + endc
+
+def purple(text):
+    return Fore.MAGENTA + str(text) + endc
+
+def yellow(text):
+    return Fore.YELLOW + str(text) + endc
+
+def underline(text):
+    return '\033[4m' + str(text) + endc
+
+def bold(text):
+    return '\033[1m' + str(text) + endc
 
 
 def yes_no(s=''):
     """Prompt user for a yes or no response."""
     while True:
-        response = input(s + ' (Y/N)')
+        response = input(s + ' (Y/N):')
         if response.lower() == 'y':
             return True
         elif response.lower() == 'n':
             return False
         else:
             print('Use the \'Y\'/\'N\' keys to choose Yes or No')
-
+    
 
 def exit_app(exit_message=''):
     """Display exit message, then wait for user to press enter before running exit()"""
-    print(red + exit_message + endc)
+    print(red(exit_message))
     input('\nPress Enter to exit the program')
     exit()
-
+    
 
 def test(*variables, inline=False):
     """Print the list of parameters with some helpful formatting.
 
     Option to print iterables horizontally, 'inline'. If no parameters are given, test() will print 'here'."""
+    colorama.reinit()
     if len(variables) == 0:
-        print(blue + 'here' + endc)
+        print(blue('here'))
+        
         return
 
     for i in range(0, len(variables)):
         curr = variables[i]
-        printstring = bold + purple + f'var {i}:\t' + endc
+        printstring = bold(purple(f'var {i}:\t'))
         if not isinstance(curr, Iterable) or isinstance(curr, str):
-            printstring += yellow + f'{variables[i]}' + endc
+            printstring += yellow(f'{variables[i]}')
         else:
-            enumeration_string = bold + blue + ', ' if inline else '\n\t\t'
+            enumeration_string = bold(blue(', ' if inline else '\n\t\t'))
             index = 0
             for x in curr:
-                printstring += ('' if index == 0 else enumeration_string) + yellow + str(x) + endc
+                printstring += ('' if index == 0 else enumeration_string) + yellow(str(x))
                 index += 1
         print(printstring)
-    print(endc)
-    return
-
+    
 
 def ptest(*variables, inline=False):
     """ Pass all parameters to test() and then pause execution until user presses enter."""
     test(*variables, inline=inline)
-    print(red)
-    input('Process paused. Press any key to un-pause')
-    print(endc)
+    input(red('Process paused. Press any key to un-pause'))
+
+    
+
+exit_app(('sdfdsfsdf'))

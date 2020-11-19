dfd0fcca026b945b10058131a8bde62a6a40eb95
Jeremy <jdean3003@gmail.com>
2020-11-18 22:45:24 -0500

    modified test() to add support for dictionaries, as well as to improve visual formatting and add documentation

--- jtools/jconsole.py
+++ jtools/jconsole.py
@@ -64,26 +64,49 @@ def test(*variables):
     colorama.reinit()
     if len(variables) == 0:
         print(bold(blue('here')))
-        return
     else:
         print(_recursively_add_vars(variables))
 
 def _recursively_add_vars(iterable, label_index='', indent_lvl=0):
-    """Recursively add iterables to the printstring.
+    """Recursive into iterables and add their values to the printstring that test() will ultimately display.
 
     This function is only called from within test()"""
-    indent = '    '
     printstring = ''
-    for i in range(len(iterable)):
+    BASE_INDENT = '    '
+    iterable_is_dictionary = isinstance(iterable, dict)
+    if iterable_is_dictionary:
+        keys = sorted(list(iterable.keys()))
+    # The first call of recursively...() is always done by test() which always passes in its *variables parameter.
+    # *variables is an iterable so there is no 'isIterable' check necessary at this outer scope. 
+    for i in range(len(iterable)): 
+        
+        # the 0th indentation lvl refers to the top lvl variables that were actually passed to the test() function.
+        # These will be labeled in the display as " test_var 2: " and so on. 
         if indent_lvl == 0:
-            printstring += bold(purple(f'var {i}:\n'))
-        curr_value = iterable[i]
-        if isinstance(curr_value, str) or not isinstance(curr_value, Iterable):
-            if indent_lvl < 2:
-                printstring += indent + yellow(str(curr_value)) + '\n'
+            printstring += bold(purple(f'test_var {i}:\n'))
+
+        # curr_value is the single object 'under consideration' (for a given loop interation of a given function call)
+        # It will be checked to see if it is a simple value that's okay to print, or if it is an iterable that needs to be recursed into further. 
+        # note: dictionaries cannot be indexed by just i, so a list of the dictionary's keys is made which CAN be indexed by i. 
+        curr_value = iterable[keys[i]] if iterable_is_dictionary else iterable[i]
+        
+        # This is the recursive function's base case. It stops recursing when it finds a string or non-iterable
+        # because it makes sense to print these primitives 'as-is'
+        curr_value_is_primitive = isinstance(curr_value, str) or not isinstance(curr_value, Iterable)
+        
+        indent_str = BASE_INDENT * indent_lvl 
+        if iterable_is_dictionary: # we always want to print dictionary keys, even if their value is another iterable
+           printstring += f'{indent_str}{cyan("key:")}{yellow(keys[i])}{cyan(" value:")} '
+           if not(curr_value_is_primitive):
+               printstring += '\n' # don't want to start printing a lower nested iterable on the same line as the upper dictionary key
+        if curr_value_is_primitive:
+            if iterable_is_dictionary: #special formatting required for dicitonaries
+                printstring += f'{yellow(iterable[keys[i]])}\n'
             else:
-                printstring += (indent * indent_lvl) + yellow(str(curr_value)) + '\n'
-        else:
+                printstring += indent_str + yellow(str(curr_value)) + '\n'
+            
+        # Continue recursion if curr_value is an iterable. 
+        if not(curr_value_is_primitive):
             printstring += _recursively_add_vars(curr_value, label_index=i, indent_lvl=indent_lvl + 1)
     return printstring
 

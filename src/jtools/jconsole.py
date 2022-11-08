b348b7b7edf963c78ca6dbea3212539aac538923
umbral-tension <jdean3003@Gmail.com>
2022-11-08 00:27:52 -0600

    added optional vertical tracer lines to improve readability of output.
    pulled text formatting out of _recursively_add_vars() and into its
    own function called _build_line()

--- src/jtools/jconsole.py
+++ src/jtools/jconsole.py
@@ -67,17 +67,22 @@ def zen(message='here', title=f'debug info'):
     """Use zenity on linux to display a popup window showing the content of message."""
     os.system(f'zenity --title "{title}" --info --text "{message}"')
 
-def test_(*variables, indent=2, tracers=True):
+def test_(*variables, indent=4, tracers=True, truncate=True):
     """returns the pretty-print string from test() instead of printing it."""
-    return _recursively_add_vars(variables)
+    return _recursively_add_vars(variables, indent=indent, tracers=tracers, truncate=truncate)
 
-def test(*variables, indent=4, tracers=True):
-    """pretty print all items in *variables."""
+def test(*variables, indent=4, tracers=True, truncate=True):
+    """pretty print all items in *variables.
+    
+    @param indent: size of indent to use for nested items
+    @param tracers: if true, vertical alignment lines are added for readability
+    @param truncate: attempt to fit the printed content to the console window better
+    """
     colorama.reinit()
     if len(variables) == 0:
         print(bold(blue('here')))
     else:
-        print('\n'.join(_recursively_add_vars(variables, indent=indent, tracers=tracers)))
+        print('\n'.join(_recursively_add_vars(variables, indent=indent, tracers=tracers, truncate=truncate)))
 
 def ptest(*variables, inline=False):
     """ Pass all parameters to test() and then pause execution until user presses enter."""
@@ -85,17 +90,17 @@ def ptest(*variables, inline=False):
     input(red('Process paused. Press any key to un-pause'))
 
 
-def _recursively_add_vars(iterable, list_of_lines = None, indent_lvl=0, indent=4, tracers=True):
-    """Recurse into iterables, adding their values to the printstring that test() will ultimately display.
-
-    This function is only called from within test()"""
+def _recursively_add_vars(iterable, list_of_lines = [], indent_lvl=0, droplines=[], indent=4, tracers=True, truncate=True):
+    """Recurse into iterables, adding their values to the printstring that test() will ultimately display. This function 
+    is only called from within test()
+    
+    @param droplines: list of indent lvls. Tracks which indentation lvls currently need tracer dots.
+    """
     lol = list_of_lines
-    if lol is None:
-        lol = []
     indent_str = ' ' * indent * indent_lvl
     iterable_is_dictionary = isinstance(iterable, dict)
-    if iterable_is_dictionary:
-        keys = list(iterable.keys())
+    keys = list(iterable.keys()) if iterable_is_dictionary else []
+    
     # The first call of _recursively...() is always done by test() which always passes in its *variables parameter.
     # *variables is an iterable so there is no 'isIterable' check necessary at this outer scope. 
     for i in range(len(iterable)): 
@@ -105,52 +110,76 @@ def _recursively_add_vars(iterable, list_of_lines = None, indent_lvl=0, indent=4
         if indent_lvl == 0:
             lol.append(bold(purple(f'var {i}:')))
 
-        # curr_value is the single object 'under consideration' (for a given loop iteration of a given function call)
-        # It will be checked to see if it is a simple value that's okay to print, or if it is an iterable that needs to
-        # be recursed into further. # note: dictionaries cannot be indexed by just i, so a list of the dictionary's
-        # keys is made which CAN be indexed by i.
-        if iterable_is_dictionary:
-            curr_value = iterable[keys[i]]
-        else:
-            curr_value = iterable[i]
-        
-        # This is the recursive function's base case. It stops recursing when it finds a string or non-iterable
-        # because it makes sense to print these primitives 'as-is'
-        # TODO Look into __get_item__ implementation to support recursion into non-standard iterables. Currently only
-        #  simple iterables are recursed into eg (list/dict/set/tuple).
+        # curr_value is the single object 'under consideration'.
+        curr_value = iterable[keys[i]] if iterable_is_dictionary else iterable[i]
         curr_value_is_primitive = is_primitive(curr_value)
-        curr_val_type = str(type(curr_value)).replace('class', '').replace("'", "").replace(' ', '')
 
-        keyval_str = ''
-        if iterable_is_dictionary:  # we always want to print dictionary keys, even if their associated value is another iterable
-            keyval_str += f'{indent_str}{cyan("key:")}{yellow(keys[i])}{cyan(" value:")}'
-            if not curr_value_is_primitive:
-                # print type of the current key's value (i.e. the type of the upcoming iterable)
-                # also end line because we don't want to start printing a lower nested iterable on the same line as the upper dictionary key
-                keyval_str += f'{bold(curr_val_type)}'
-                lol.append(keyval_str)
+        # build formatted print line
+        line = _build_line(iterable, iterable_is_dictionary, keys, i, curr_value, \
+            curr_value_is_primitive, indent_lvl, indent, tracers, droplines, truncate)
+        if not line is None:
+            lol.append(line)
+
+        # Continue recursion if curr_value is not a primitive. 
+        if not curr_value_is_primitive:
+            if i < len(iterable)-1 and indent_lvl != 0:
+                droplines.append(indent_lvl)
+            lol = _recursively_add_vars(curr_value, list_of_lines=lol, indent_lvl=indent_lvl+1,\
+                 indent=indent, tracers=tracers, droplines=droplines, truncate=truncate) 
+    try:
+        droplines.remove(indent_lvl)
+    except:
+        pass
+    return lol
+
+
+def _build_line(iterable, iterable_is_dictionary, keys, curr_index, curr_value,
+ curr_value_is_primitive, indent_lvl, indent, tracers, droplines, truncate):
+    """Produce a nicely formatted line of text to be used by _recursively_add_vars()"""
+    i = curr_index
+    curr_val_type = str(type(curr_value)).replace('class ', '').replace("'", "")
+    
+    # build indentation string w or w/o tracers
+    indent_str = ' ' * indent * indent_lvl
+    if tracers:
+        indent_str = list(indent_str)
+        for x in droplines:
+            if x != indent_lvl:
+                dot = purple('·') # middle dot char is U+00B7
+                dropindex = (x * indent)
+                try:
+                    indent_str[dropindex] = dot
+                except:
+                    pass
+        indent_str = ''.join(indent_str)
+    
+    
+    line = ''
+    # Seperate formatting instructions for dictionaries
+    if iterable_is_dictionary:
+        # we always want to print dictionary keys, even if their associated value is another iterable
+        line = f'{indent_str}{cyan("key:")}{yellow(keys[i])}{cyan(" value:")}'
+        if curr_value_is_primitive:
+            line += f'{yellow(str(curr_value))}'
+        else:
+            line += f'{curr_val_type}'
+    else:
         if curr_value_is_primitive:
             if indent_lvl == 0:
                 indent_str += ' ' * indent
-            if iterable_is_dictionary:  # special formatting required for dictionaries
-                keyval_str += f'{yellow(iterable[keys[i]])}'
-                lol.append(keyval_str)
-            elif isinstance(iterable, list):
-                lol.append(indent_str + cyan(f'{i}|') + yellow(str(curr_value)))
+            if isinstance(iterable, list):
+                line = indent_str + cyan(f'{i}|') + yellow(str(curr_value))
             else:
-                lol.append(indent_str + yellow(str(curr_value)))
-        
-        # Continue recursion if curr_value is an iterable. 
-        if not curr_value_is_primitive:
-            # type info will already have been printed if iterable is a dictionary. 
-            if not iterable_is_dictionary and indent_lvl != 0:
-                lol.append(f'{indent_str}{cyan(f"{i}|")}{bold(curr_val_type)}')
-            lol = _recursively_add_vars(curr_value, list_of_lines=lol, indent_lvl=indent_lvl+1)
-    
-    # prep lol to be returned to test() function. 
-    if indent_lvl == 0:
-        pass#ol = ''.join(lol)
-    return lol
+                line = indent_str + yellow(str(curr_value))
+        else: 
+            if indent_lvl != 0:
+                line = f'{indent_str}{cyan(f"{i}|")}{curr_val_type}'
+            else:
+                return None
+    return line
+
+
+
 
 import builtins
 builtin_types = [getattr(builtins, d) for d in dir(builtins) if isinstance(getattr(builtins, d), type)]

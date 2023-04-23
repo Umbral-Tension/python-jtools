d35028eee9c2e0dcb6dc22fffd83c5af6d88e9cd
jeremy <>
2023-04-22 22:53:15 -0500

    fixed path in jconsole_test.py

--- src/jtools/jconsole_test.py
+++ src/jtools/jconsole_test.py
@@ -5,13 +5,13 @@ import os.path
 BASEDIR = os.path.dirname(__file__)
 
 
-with open(os.path.join(BASEDIR, 'mixed_list_dict.json'), 'r') as f:
+with open(os.path.join(BASEDIR, '../../resources/mixed_list_dict.json'), 'r') as f:
     mixed_list_dict = json.load(f)
 
-with open(os.path.join(BASEDIR, 'deep_nested_dict.json'), 'r') as f:
+with open(os.path.join(BASEDIR, '../../resources/deep_nested_dict.json'), 'r') as f:
     deep_nested_dict = json.load(f)
 
-with open(os.path.join(BASEDIR, 'list of dictionaries.json'), 'r') as f:
+with open(os.path.join(BASEDIR, '../../resources/list of dictionaries.json'), 'r') as f:
     list_of_dicts = json.load(f)
 
 

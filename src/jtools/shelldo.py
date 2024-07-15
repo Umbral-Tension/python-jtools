5c2c23307a23f9ca2897733bd6f1b1d54a785543
jeremy <>
2024-07-14 22:52:42 -0500

    changed logging and stdout/stderr behavior and shelldo now requires a logdir in constructor.

--- src/jtools/shelldo.py
+++ src/jtools/shelldo.py
@@ -4,13 +4,13 @@ import os, shutil, sys
 import os.path as opath
 import shlex
 from shlex import split as lex
-from subprocess import Popen, PIPE, run
+from subprocess import Popen, PIPE, run, STDOUT
 import traceback
 from datetime import datetime
 
 class Shelldo:
-    def __init__(self, logdir=None):
-        """Run sets of shell commands with chain() loging the results.
+    def __init__(self, logdir):
+        """Run sets of shell commands with chain(), logging the results.
         Allows to build a human readable list of actions and results that can 
         be printed as they're happening or as a full report all at once. 
 
@@ -24,19 +24,14 @@ class Shelldo:
         @param logdir: path where logs should be stored, defaults to cwd. 
         """ 
         
-        self.logdir = os.getcwd() if logdir is None else logdir
+        self.logdir = logdir
         os.makedirs(self.logdir, exist_ok=True)
 
         timestamp = str(datetime.now())
         timestamp = timestamp[:timestamp.rfind('.')]
         self.logfile = f'{self.logdir}/shelldo log {timestamp}'
-        try:
-            latestlog = f'{self.logdir}/@LATEST-SHELLDO-LOG.txt'
-            os.remove(latestlog)
-        except FileNotFoundError:
-            pass
-        os.symlink(self.logfile, latestlog)
-        
+        self.full_transcript = f'{self.logdir}/shelldo full transcript {timestamp}'
+
         # determine platform info 
         if os.name is None:
             self.package_manager = None
@@ -59,8 +54,7 @@ class Shelldo:
         if not uninstall:
             return f'sudo {self.package_manager} -y install {program}'
         if uninstall:
-            actions = {'dnf': 'remove', 'apt': 'remove'}
-            return f'sudo {self.package_manager} -y {actions[self.package_manager]} {program}'
+            return f'sudo {self.package_manager} -y remove {program}'
 
     def set_action(self, description, noprint=False, nocolor=False):
         """Update and print the current action."""
@@ -74,9 +68,8 @@ class Shelldo:
                 starting = jc.bold(jc.green('starting'))
                 print(f'[{starting}]:  {self.curraction}')
 
-    def set_result(self, result, action:str=None, nocolor=False, norecord=False, noprint=False):
-        """record and/or print the specified result of the specified action.
-        Action defaults to self.curraction. 
+    def set_result(self, result, action=None, nocolor=False, noprint=False):
+        """record the result of the specified action. Action defaults to self.curraction. 
         
         @param result: may be specified with the strings "ok/fail" or booleans. 
         @param action: value to lookup in self.actions and record a result for.
@@ -87,23 +80,29 @@ class Shelldo:
         result = self._parseresult(result)
         if action is None:
             action = self.curraction
-        if not norecord:
-            for x in self.actions:
-                if x[0].casefold() == action.casefold():
-                    x.append(result)
+        for x in self.actions:
+            if x[0].casefold() == action.casefold():
+                x.append(result)
         if not noprint:
-            if nocolor:
-                print(f'[{result}]:  {action}\n')
-            else:
-                result = jc.green(result) if result == "OK" else jc.red(result)
-                print(f'[{jc.bold(result)}]:  {action}\n')
+            self.print_action(result, action, nocolor=nocolor)
+
     
 
+    def print_action(self, result, action, nocolor=False):
+        """"print the specified action/result combo. Does not reference shelldo object's action list. Merely prints the string arguments given.
+        """
+        if nocolor:
+                print(f'[{result}]:  {action}\n')
+        else:
+            result = jc.green(result) if result == "OK" else jc.red(result)
+            print(f'[{jc.bold(result)}]:  {action}\n')
+
+            
     def report(self):
         """Print all actions and their results."""
         print(jc.bold(jc.yellow('\n////// Report /////')))
         for x in self.actions:
-            self.set_result(x[1], x[0], norecord=True)
+            self.print_action(x[1], x[0])
         
 
     def log(self, result, action):
@@ -113,8 +112,7 @@ class Shelldo:
         """
         result = self._parseresult(result)
         with open(self.logfile, 'a') as f:
-            line = f"[{result}]:  {action}\n"
-            f.writelines(line)
+            f.writelines(f"[{result}]:  {action}\n")
 
     def _parseresult(self, result):
         if isinstance(result, str):
@@ -122,11 +120,12 @@ class Shelldo:
         return {True:"OK", False:"FAIL", "ok": "OK", "fail": "FAIL"}[result]
 
 
-    def chain(self, cmds, logall=False, ignore_exit_code=False, quiet=False):
+    def chain(self, cmds:list, logall=False, ignore_exit_code=False, quiet=False):
         """run a series of commands in the shell, returning False immediately
          if one in the series exits with any non-zero value. Otherwise returns True.
+         IO redirection does not work with this method. 
 
-        commands are run usingsubprocess.Popen(bufsize=1, stdout=PIPE, stderr=PIPE, text=True)
+        commands are run using subprocess.Popen(bufsize=1, stdout=PIPE, stderr=PIPE, text=True)
 
         
         @param cmds: list of strings, each of which is a single shell command,
@@ -136,33 +135,32 @@ class Shelldo:
         @param quiet: don't print stdout content as the subprocess makes it 
         available. 
         """
-        
-        for x in cmds:
-            try:
-                cmd = shlex.split(x)
-                with Popen(cmd, bufsize=1, stdout=PIPE, stderr=PIPE, text=True) as p:
-                    # print stdout
-                    if not quiet:
-                        for line in p.stdout:
-                            print(line, end='') 
-                    # wait for subprocess to finish
-                    p.wait()
-                    if p.returncode != 0:
-                        print(f'chain: failed (exit code {p.returncode}): {x}')
-
-                        errstr = ''.join([x for x in p.stderr])
-                        self.log(False,
-                                f'failed (exit code {p.returncode}): {x}\n\tstderr:\n\t\t{errstr}')        
-                        if not ignore_exit_code:
-                            return False
-                    else:
-                        if logall:
-                            self.log(True, x)
-            except:
-                estring = traceback.format_exc()
-                print(estring)
-                self.log(False, f'{x}\n\t{estring}')
-                return False
+        with open(self.full_transcript, 'a') as ft:
+            ft.writelines(f'ACTION: {self.curraction}\n')
+            
+            for x in cmds:
+                ft.writelines(f'\tCMD: {x}\n')
+                try:
+                    cmd = shlex.split(x)
+                    with Popen(cmd, bufsize=1, stdout=PIPE, stderr=STDOUT, text=True) as p:
+                        # print stdout
+                        if not quiet:
+                            for line in p.stdout:
+                                ft.writelines(f'\t\t{line}\n')
+                                print(line, end='') 
+                        # wait for subprocess to finish
+                        p.wait()
+                        if p.returncode != 0 and not ignore_exit_code:   
+                                return False
+                        else:
+                            if logall:
+                                self.log(True, x)
+                except:
+                    estring = traceback.format_exc()
+                    print(estring)
+                    ft.writelines(['\t\tPYTHON EXCEPTION:\n\t\t\t'] + ['\n\t\t\t'.join(estring.split('\n'))] + ['\t\t\t\n'])
+                    self.log(False, f'{x}\n\t{estring}')
+                    return False
             
         return True
 
